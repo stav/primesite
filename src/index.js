ec8cca02e7dd79183f7d6c3fee7ec04fba07ec60
Steven Almeroth <sroth77@gmail.com>
2020-03-28 17:24:39 -0400

    express handlebars

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const port = 5005;
+const app = express();
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    greeting: 'Welcome to my Home page'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
