cbcaa61a0de81d69fd4fb80a35edf7080b4dbdc2
Steven Almeroth <sroth77@gmail.com>
2020-04-18 22:37:02 -0400

    denormalizing script

--- src/static/dom.js
+++ src/static/dom.js
@@ -10,3 +10,23 @@ function focusContactName () {
   // For some reason Firefox need the timeout
   setTimeout('document.getElementById("contact-name").focus()', 500)
 }
+
+function closeModal( id ) {
+
+  document.getElementById( id ).classList.remove('is-active')
+
+  // These overflows are set by modal-fx.js
+  document.getElementsByTagName('html')[0].style.overflow = "";
+  document.getElementsByTagName('body')[0].style.overflowY = "";
+
+}
+
+function closeAllModals() {
+
+  var modals = document.getElementsByClassName('modal');
+
+  for (let $modal of modals) {
+    closeModal( $modal.id )
+  }
+
+}
--- src/static/portfolio.js
+++ src/static/portfolio.js
@@ -24,20 +24,3 @@ function setModalImage( url ) {
   })
 
 }
-
-function closePortfolio() {
-
-  document.getElementById('portfolio-card').classList.remove('is-active')
-
-  // These overflows are set by modal-fx.js
-  document.getElementsByTagName('html')[0].style.overflow = "";
-  document.getElementsByTagName('body')[0].style.overflowY = "";
-
-}
-
-function contactPortfolio() {
-
-  closePortfolio()
-  focusContactName()
-
-}
